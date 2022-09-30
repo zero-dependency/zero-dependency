b5aec5969e7dea29b4c823160e34cd7a87504313
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-09-30 15:27:09 +0800

    refactor

--- packages/storage/src/storage.ts
+++ packages/storage/src/storage.ts
@@ -1,3 +1,4 @@
+import { BaseStorage } from './base-storage.js'
 import type { ExcludeFunction } from './types.js'
 
 export function LocalStorage<T>(
@@ -12,41 +13,4 @@ export function SessionStorage<T>(
   value: ExcludeFunction<T>
 ): BaseStorage<T> {
   return new BaseStorage<T>(key, value, sessionStorage)
-}
-
-class BaseStorage<T> {
-  constructor(
-    private readonly key: string,
-    public readonly initialValue: T,
-    private readonly storage: Storage
-  ) {}
-
-  values(): T {
-    try {
-      const value = this.storage.getItem(this.key)
-      return value ? JSON.parse(value) : this.initialValue
-    } catch {
-      return this.initialValue
-    }
-  }
-
-  write(value: T): T
-  write(value: (prevValue: T) => T): T
-  write(values: T | ((prevValue: T) => T)): T | null {
-    if (values instanceof Function) {
-      values = values(this.values())
-    }
-
-    try {
-      this.storage.setItem(this.key, JSON.stringify(values))
-    } catch (err) {
-      console.error('Failed to save:', err)
-    } finally {
-      return values
-    }
-  }
-
-  reset(): void {
-    this.write(this.initialValue)
-  }
 }
