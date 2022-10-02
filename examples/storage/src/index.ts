b1129f429e243215ab37338d8e5cbfc4cf17ba3d
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-03 01:05:57 +0800

    update storage example

--- examples/storage/src/index.ts
+++ examples/storage/src/index.ts
@@ -5,10 +5,10 @@ interface User {
   name: string
 }
 
-const storage = SessionStorage<User[]>('users', [])
+const storage = new SessionStorage<User[]>('users', [])
 storage.write((users) => [
   ...users,
   { id: users.length + 1, name: Math.random().toString(16).slice(2) }
 ])
 
-console.log(storage.values())
+console.log(storage.values)
