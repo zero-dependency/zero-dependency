658d32f0e1fec174090ffa00441676a86e25e610
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-17 01:32:33 +0800

    upstream

--- examples/cookie/src/index.ts
+++ examples/cookie/src/index.ts
@@ -1,9 +1,22 @@
-import { cookies } from '@zero-dependency/cookie'
+import { Cookie } from '@zero-dependency/cookie'
 
 interface User {
   id: number
   name: string
 }
+
+const cookies = new Cookie({
+  serialize(value) {
+    return JSON.stringify(value)
+  },
+  deserialize(value) {
+    try {
+      return JSON.parse(value)
+    } catch {
+      return null
+    }
+  }
+})
 
 cookies.set('le_xot', { id: 1, name: 'Lesha' }, { expires: 7 })
 console.log(cookies.get<User>('le_xot'))
