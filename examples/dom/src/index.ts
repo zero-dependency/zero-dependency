27925ebfe7abebf1da6c5fed722138dd90c6f469
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-30 10:30:32 +0800

    chore(examples): update dom

--- examples/dom/src/index.ts
+++ examples/dom/src/index.ts
@@ -1,9 +1,4 @@
-import {
-  el,
-  observeElement,
-  text,
-  waitObserveElement
-} from '@zero-dependency/dom'
+import { el, observeElement, text, waitElement } from '@zero-dependency/dom'
 
 function createButton() {
   const button = el(
@@ -26,12 +21,13 @@ function createButton() {
 }
 
 async function bootstrap() {
-  observeElement('.card', (node) => {
-    console.log('observeElement:', node)
-  })
-
-  const card = await waitObserveElement('.card')
-  console.log('waitObserveElement:', card)
+  observeElement(
+    '.card',
+    (node) => {
+      console.log('observeElement:', node)
+    },
+    { attributes: true }
+  )
 }
 
 createButton()
