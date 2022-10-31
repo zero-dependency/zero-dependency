f5f53f0842bdb7c06238fb682a362ad508037b62
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-10-31 18:04:27 +0800

    upstream

--- examples/dom/src/index.ts
+++ examples/dom/src/index.ts
@@ -1,36 +1,34 @@
-import { el, text, waitElement } from '@zero-dependency/dom'
+import { el, observeElement, text, waitElement } from '@zero-dependency/dom'
 
 function createButton() {
+  const card = el('div', { className: 'card' })
+
+  const container = el('div', [text('Hello World'), card])
+
   const button = el(
     'button',
     {
       onclick: () => {
-        card.classList.add(Math.random().toString(16).slice(12))
+        const value = Math.random().toString(16).slice(2)
+        // card.textContent = value
+        card.classList.add(value)
       }
     },
     'Click me'
   )
-  const card = el('div', { className: 'card' })
-  const container = el('div', [
-    text('Hello World'),
-    card,
-    button
-  ])
 
-  document.body.appendChild(container)
+  document.body.append(button)
+
+  setTimeout(() => {
+    const cards = Array.from({ length: 10 }, () => card.cloneNode(true))
+    document.body.append(...cards)
+  }, 1000)
 }
 
 async function bootstrap() {
-  // observeElement(
-  //   '.card',
-  //   (node) => {
-  //     console.log('observeElement:', node)
-  //   },
-  //   { attributes: true }
-  // )
-
   const card = await waitElement('.card')
   console.log('waitElement:', card)
+  observeElement(card, (mutation) => console.log('observeElement:', mutation))
 }
 
 createButton()
