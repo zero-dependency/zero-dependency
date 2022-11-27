24e71c268e3871fc37bf7314fab52b0d8ecb8d5c
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-11-27 17:50:51 +0800

    chore(examples): update fetcher

--- examples/fetcher/src/extends.ts
+++ examples/fetcher/src/extends.ts
@@ -1,6 +1,7 @@
 import { Fetcher } from '@zero-dependency/fetcher'
+import type { FetcherInterceptor } from '@zero-dependency/fetcher'
 
-interface Response {
+interface JSONPlaceholderResponse {
   body: string
   email: string
   id: number
@@ -8,17 +9,33 @@ interface Response {
   postId: number
 }
 
-export function extendsApp() {
+export async function extendsApp() {
   const headers = new Headers()
   headers.set('Content-Type', 'application/json')
 
-  const baseApi = new Fetcher('https://jsonplaceholder.typicode.com', { headers })
+  const baseApi = new Fetcher('https://jsonplaceholder.typicode.com', {
+    headers
+  })
 
-  headers.set('Authorization', 'xxx')
-  const postsApi = baseApi.extends('/posts', { headers })
-  postsApi.get<Response>('/1').then((res) => console.log('/posts/1', res))
+  const i: FetcherInterceptor.Response = {
+    async response(response) {
+      console.log(await response.json())
+      console.log({ response })
+    },
+    responseError(responseError) {
+      console.log({ responseError })
+    }
+  }
 
-  headers.delete('Authorization')
-  const commentsApi = baseApi.extends('/comments', { headers })
-  commentsApi.get<Response>('/1').then((res) => console.log('/comments/1', res))
+  baseApi.interceptor.register(i)
+
+  await baseApi.get<JSONPlaceholderResponse>('/comments/1')
+
+  // headers.set('Authorization', 'xxx')
+  // const postsApi = baseApi.extends('/posts', { headers })
+  // postsApi.get<PlaceholderResponse>('/1').then((res) => console.log('/posts/1', res))
+
+  // headers.delete('Authorization')
+  // const commentsApi = baseApi.extends('/comments', { headers })
+  // commentsApi.get<PlaceholderResponse>('/1').then((res) => console.log('/comments/1', res))
 }
