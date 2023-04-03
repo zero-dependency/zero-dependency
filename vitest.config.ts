2ba9912e3f3c55a754d50cdc26bdcf13b41eeed0
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-04-03 20:32:57 +0800

    upstream

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     watch: false,
+    silent: true,
     threads: true,
     logHeapUsage: true,
     environment: 'jsdom'
