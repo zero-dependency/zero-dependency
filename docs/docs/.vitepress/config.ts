d8482a944c24788814b0cd6e6f80285feafb017b
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-06-06 00:15:43 +0800

    chore: disable cleanUrls

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   lang: 'en-US',
   title: 'Zero Dependency',
   description: 'Zero Dependency',
-  cleanUrls: true,
+  // cleanUrls: true,
   lastUpdated: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
