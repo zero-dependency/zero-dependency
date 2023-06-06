9af20ed8280be510c4743cb24a6928ed3c41c081
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-06-06 23:59:32 +0800

    fix(docs): baseUrl

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
   url: 'https://zero-dependency.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/zero-dependency/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
